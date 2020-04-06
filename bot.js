58580b6d1f7ede2eb040a879ed49bbb237b1091f
Ted Yarmoski <ted@yarmoski.com>
2020-04-05 17:35:34 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -39,67 +39,67 @@ fb.get.posts
   })
   .catch(error => console.error(error));
 
-// const myTask = new alpha.Task()
-//   .to(fb) //the alphaFB instance we configured earlier
-//   .do(actionObject => {
-//     actionObject.type = "post";
-//     actionObject.message = postContents;
-//     actionObject.done();
-//   })
-//   .every(10)
-//   .second()
-//   .start();
+const myTask = new alpha.Task()
+  .to(fb) //the alphaFB instance we configured earlier
+  .do(actionObject => {
+    actionObject.type = "post";
+    actionObject.message = postContents;
+    actionObject.done();
+  })
+  .every(10)
+  .second()
+  .start();
 
-// FB.api("me/feed", "post", { message: postContents }, res => {
-//   if (!res || res.error) {
-//     return console.error(!res ? "error occurred" : res.error);
-//   }
-//   console.log(`Post ID: ${res.id}`);
-// });
+FB.api("me/feed", "post", { message: postContents }, res => {
+  if (!res || res.error) {
+    return console.error(!res ? "error occurred" : res.error);
+  }
+  console.log(`Post ID: ${res.id}`);
+});
 
-// // get all posts / comments / reactions (t)
-// //id = page id
-// //f = fields
-// let getAll = (t, id, f, cb = () => {}, res = null, arr = []) => {
-//   let af = "";
-//   if (res && res.paging && res.paging.next) {
-//     let next = res.paging.next.match(/after=(.*)/)[0];
-//     af = "&" + next;
-//   }
-//   FB.api(`${id}/${t}?limit=100${af}${f}`, (res, err) => {
-//     if (err) {
-//       return;
-//     } else {
-//       if (!res.data.length && !arr.length) {
-//         cb(arr);
-//         return;
-//       }
-//       if (af.length == 0 && arr.length > 1) {
-//         cb(arr);
-//         return;
-//       } else {
-//         if (!arr.length) arr = res.data;
-//         else arr = arr.concat(res.data);
-//         if (
-//           res.paging.cursors.before == res.paging.cursors.after &&
-//           res.paging.cursors != undefined
-//         )
-//           cb(arr);
-//         else getAll(t, id, f, cb, res, arr);
-//       }
-//     }
-//   });
-// };
+// get all posts / comments / reactions (t)
+//id = page id
+//f = fields
+let getAll = (t, id, f, cb = () => {}, res = null, arr = []) => {
+  let af = "";
+  if (res && res.paging && res.paging.next) {
+    let next = res.paging.next.match(/after=(.*)/)[0];
+    af = "&" + next;
+  }
+  FB.api(`${id}/${t}?limit=100${af}${f}`, (res, err) => {
+    if (err) {
+      return;
+    } else {
+      if (!res.data.length && !arr.length) {
+        cb(arr);
+        return;
+      }
+      if (af.length == 0 && arr.length > 1) {
+        cb(arr);
+        return;
+      } else {
+        if (!arr.length) arr = res.data;
+        else arr = arr.concat(res.data);
+        if (
+          res.paging.cursors.before == res.paging.cursors.after &&
+          res.paging.cursors != undefined
+        )
+          cb(arr);
+        else getAll(t, id, f, cb, res, arr);
+      }
+    }
+  });
+};
 
-// FB.api(
-//   "/your-object-id",
-//   "GET",
-//   { fields: "reactions.type(LOVE).limit(0).summary(total_count)" },
-//   function(response) {
-//     console.log(response);
-//   }
-// );
+FB.api(
+  "/your-object-id",
+  "GET",
+  { fields: "reactions.type(LOVE).limit(0).summary(total_count)" },
+  function(response) {
+    console.log(response);
+  }
+);
 
-// getAll("reaction", 103793077750716_117302276399796, "", function(response) {
-//   console.log(response);
-// });
+getAll("reaction", 103793077750716_117302276399796, "", function(response) {
+  console.log(response);
+});
